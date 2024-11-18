bbd63011c4cd3e07bb8202fc56ca0d064d66392f
Nastja Demchyk <NastjaDemchyk@gmail.com>
2024-11-18 20:44:51 +0100

    changes in HomePage

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import s from "./NotFoundPage.module.css";
 
 const NotFoundPage = () => {
   return (
     <div>
-      <div>Page not found</div>
-      <Link to="/">Home</Link>
+      <div>Page is not found...</div>
+      <NavLink to="/" className={s.navLink}>
+        Home
+      </NavLink>
     </div>
   );
 };
