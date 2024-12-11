435be9e69efd8f59b72df74617e2ef98f6bb3d5b
Nastja Demchyk <NastjaDemchyk@gmail.com>
2024-12-11 20:23:53 +0100

    added stules to page not found

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -5,7 +5,7 @@ import s from "./NotFoundPage.module.css";
 const NotFoundPage = () => {
   return (
     <div>
-      <div>Page is not found...</div>
+      <div className={s.text}>Page is not found...</div>
       <NavLink to="/" className={s.navLink}>
         Home
       </NavLink>
